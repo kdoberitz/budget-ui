5074de2b3a7f43768cf2ff97c7d4fabd65659fb9
Kilian Doberitz <131739200+kdoberitz@users.noreply.github.com>
2024-11-16 21:21:02 +0100

    Add Kategorie erstellen

--- src/app/category/component/category-modal/category-modal.component.ts
+++ src/app/category/component/category-modal/category-modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, ViewChild } from '@angular/core';
 import {
   IonButton,
   IonButtons,
@@ -13,11 +13,17 @@ import {
   IonList,
   IonTitle,
   IonToolbar,
-  ModalController
+  ModalController,
+  ViewDidEnter
 } from '@ionic/angular/standalone';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { addIcons } from 'ionicons';
 import { close, save, text, trash } from 'ionicons/icons';
+import { CategoryService } from '../../service/category.service';
+import { LoadingIndicatorService } from '../../../shared/service/loading-indicator.service';
+import { ToastService } from '../../../shared/service/toast.service';
+import { CategoryUpsertDto } from '../../../shared/domain';
+import { finalize } from 'rxjs';
 
 @Component({
   selector: 'app-category-modal',
@@ -42,9 +48,20 @@ import { close, save, text, trash } from 'ionicons/icons';
     IonList
   ]
 })
-export default class CategoryModalComponent {
+export default class CategoryModalComponent implements ViewDidEnter {
   // DI
+  private readonly categoryService = inject(CategoryService);
+  private readonly formBuilder = inject(FormBuilder);
+  private readonly loadingIndicatorService = inject(LoadingIndicatorService);
   private readonly modalCtrl = inject(ModalController);
+  private readonly toastService = inject(ToastService);
+
+  readonly categoryForm = this.formBuilder.group({
+    id: [null! as string], // hidden
+    name: ['', [Validators.required, Validators.maxLength(40)]]
+  });
+
+  @ViewChild('nameInput') nameInput?: IonInput;
 
   constructor() {
     // Add all used Ionic icons
@@ -56,10 +73,26 @@ export default class CategoryModalComponent {
   }
 
   save(): void {
-    this.modalCtrl.dismiss(null, 'save');
+    this.loadingIndicatorService.showLoadingIndicator({ message: 'Saving category' }).subscribe(loadingIndicator => {
+      const category = this.categoryForm.value as CategoryUpsertDto;
+      this.categoryService
+        .upsertCategory(category)
+        .pipe(finalize(() => loadingIndicator.dismiss()))
+        .subscribe({
+          next: () => {
+            this.toastService.displaySuccessToast('Category saved');
+            this.modalCtrl.dismiss(null, 'refresh');
+          },
+          error: error => this.toastService.displayWarningToast('Could not save category', error)
+        });
+    });
   }
 
   delete(): void {
     this.modalCtrl.dismiss(null, 'delete');
+  }
+
+  ionViewDidEnter(): void {
+    this.nameInput?.setFocus();
   }
 }
