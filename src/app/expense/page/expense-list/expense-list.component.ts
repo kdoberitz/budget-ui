057261d32c9edddd325fb1b179e1e33dd815e2a8
Kilian Doberitz <131739200+kdoberitz@users.noreply.github.com>
2024-11-24 21:39:48 +0100

    Add Ausgaben anzeigen (basic, ohne Monate)

--- src/app/expense/page/expense-list/expense-list.component.ts
+++ src/app/expense/page/expense-list/expense-list.component.ts
@@ -29,13 +29,19 @@ import {
   IonSkeletonText,
   IonTitle,
   IonToolbar,
-  ModalController
+  ModalController,
+  ViewDidEnter
 } from '@ionic/angular/standalone';
 import { ReactiveFormsModule } from '@angular/forms';
 import { addIcons } from 'ionicons';
 import { add, alertCircleOutline, arrowBack, arrowForward, pricetag, search, swapVertical } from 'ionicons/icons';
 import { CurrencyPipe, DatePipe } from '@angular/common';
 import ExpenseModalComponent from '../../component/expense-modal/expense-modal.component';
+import { ToastService } from '../../../shared/service/toast.service';
+import { Expense, ExpenseCriteria } from '../../../shared/domain';
+import { ExpenseService } from '../../service/expense.service';
+import { finalize } from 'rxjs';
+import { InfiniteScrollCustomEvent, RefresherCustomEvent } from '@ionic/angular';
 
 @Component({
   selector: 'app-expense-list',
@@ -77,9 +83,17 @@ import ExpenseModalComponent from '../../component/expense-modal/expense-modal.c
     IonButton
   ]
 })
-export default class ExpenseListComponent {
+export default class ExpenseListComponent implements ViewDidEnter {
   // DI
+  private readonly expenseService = inject(ExpenseService);
   private readonly modalCtrl = inject(ModalController);
+  private readonly toastService = inject(ToastService);
+
+  expenses: Expense[] | null = null;
+  readonly initialSort = 'name,asc';
+  lastPageReached = false;
+  loading = false;
+  searchCriteria: ExpenseCriteria = { page: 0, size: 25, sort: this.initialSort };
 
   date = set(new Date(), { date: 1 });
 
@@ -88,14 +102,53 @@ export default class ExpenseListComponent {
     addIcons({ swapVertical, pricetag, search, alertCircleOutline, add, arrowBack, arrowForward });
   }
 
-  async openExpenseModal() {
+  ionViewDidEnter(): void {
+    this.loadExpenses();
+  }
+
+  private loadExpenses(next?: () => void): void {
+    if (!this.searchCriteria.name) delete this.searchCriteria.name;
+    this.loading = true;
+    this.expenseService
+      .getExpenses(this.searchCriteria)
+      .pipe(
+        finalize(() => {
+          this.loading = false;
+          if (next) next();
+        })
+      )
+      .subscribe({
+        next: expenses => {
+          if (this.searchCriteria.page === 0 || !this.expenses) this.expenses = [];
+          this.expenses.push(...expenses.content);
+          this.lastPageReached = expenses.last;
+        },
+        error: error => this.toastService.displayWarningToast('Could not load expenses', error)
+      });
+  }
+
+  async openExpenseModal(expense?: Expense) {
     const modal = await this.modalCtrl.create({
-      component: ExpenseModalComponent
+      component: ExpenseModalComponent,
+      componentProps: { expense: expense ?? {} }
     });
     await modal.present();
+    const { role } = await modal.onWillDismiss();
+    if (role === 'refresh') this.reloadExpenses();
+  }
+
+  loadNextExpensePage($event: InfiniteScrollCustomEvent) {
+    this.searchCriteria.page++;
+    this.loadExpenses(() => $event.target.complete());
+  }
+
+  reloadExpenses($event?: RefresherCustomEvent): void {
+    this.searchCriteria.page = 0;
+    this.loadExpenses(() => $event?.target.complete());
   }
 
   addMonths = (number: number): void => {
     this.date = addMonths(this.date, number);
+    this.loadExpenses();
   };
 }
